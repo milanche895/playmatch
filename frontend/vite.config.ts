3d6b78b8aba070da9e3c156d49b83dbeaca63608
milanche895 <milanstojanovic895@gmail.com>
2026-01-07 02:30:05 +0100

    install PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
         background_color: "#ffffff",
         theme_color: "#111827",
         icons: [
-          { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png" }
+          { src: "./icons/icon-192.png", sizes: "192x192", type: "image/png" },
+          { src: "./icons/icon-512.png", sizes: "512x512", type: "image/png" }
         ]
       }
     })
